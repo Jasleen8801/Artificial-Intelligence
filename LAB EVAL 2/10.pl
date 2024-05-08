42df202c1a26f79d89bd4bbe67aa6e1515233119
Jasleen8801 <jkaur1_be21@thapar.edu>
2024-05-08 09:32:12 +0530

    ahh

--- LAB EVAL 2/10.pl
+++ LAB EVAL 2/10.pl
@@ -1,12 +1,12 @@
-loves(X,santa) :- child(X).
-loves(X,Y) :- reindeer(Y), loves(X,santa).
+loves(santa,X) :- child(X).
+loves(_,Y) :- loves(_,santa), reindeer(Y). 
 reindeer(rudolph).
 rednose(rudolph).
-% rednose(X) -> weird(X) | clown(X).
-% not(rednose(X)) | weird(X) | clown(X).
-% not(rednose(X) & not(weird(X))) | clown(X).
-% rednose(X) & not(weird(X)) -> clown(X).
-clown(X) :- rednose(X), \+weird(X).
-% no reindeer is a clown
-not(clown(X) :- reindeer(X)).
-not(loves(scrooge,X)) :- weird(X).
+% rednose(X) -> weird(X) | clown(X)
+% not(rednose(X)) | weird(X) | clown(X)
+% not(rednose(X) | not(weird(X))) | clown(X)
+% rednose(X) & not(weird(X)) | clown(X)
+clown(X) :- rednose(X), not(weird(X)).
+clown(X) :- not(reindeer(X)). 
+not_loves(scrooge,X) :- weird(X).
+not_loves(X,Y) :- not_loves(X,Y), not_loves(Y,X).
